3788e9cb6d049d34508b2a7cb067404b842891a2
Sam Thomson <samt@samt.st>
2018-12-23 16:38:05 +0700

    remove superflous

--- update-app.sh
+++ update-app.sh
@@ -8,10 +8,3 @@ docker rm $(docker stop $(sudo docker ps -aqf "name=test-name"))
 docker build -t app .
 # run the new instance
 docker run -d -p 80:80 --name=test-name app
-
-
-# docker rm $(docker stop $(docker ps -a -q --filter ancestor=<app> --format="{{.ID}}"))
-
-# docker rm $(docker stop $(sudo docker ps -aqf "name=test-name"))
-
-# sudo docker ps -aqf "name=test-app"
