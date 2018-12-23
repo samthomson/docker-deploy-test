2bceebddf5e98f6eebd93c58affe14c5268cc16c
Sam Thomson <samt@samt.st>
2018-12-23 16:02:39 +0700

    udpated

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
-    res.end('15:44')
+    res.end('16:02')
 })
 
 const PORT = 80
--- update-app.sh
+++ update-app.sh
@@ -1,4 +1,15 @@
 #!bin/bash
 
+# remove last instance
+docker rm $(docker stop $(sudo docker ps -aqf "name=test-name"))
+# build new instance
 docker build -t app .
-docker run -d -p 80:80 app
+# run the new instance
+docker run -d -p 80:80 --name=test-name app
+
+
+# docker rm $(docker stop $(docker ps -a -q --filter ancestor=<app> --format="{{.ID}}"))
+
+# docker rm $(docker stop $(sudo docker ps -aqf "name=test-name"))
+
+# sudo docker ps -aqf "name=test-app"
